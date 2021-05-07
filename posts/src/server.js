a2bd2a962b4c6220215bd25191732d44399e95bb
Marcio Junior <marcio.mj70@gmail.com>
2021-05-07 12:32:24 -0300

    posts microservice

---
+++ posts/src/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const { randomBytes } = require("crypto")
+
+const app = express()
+
+app.use(express.json())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+    return res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+        id,
+        title
+    }
+
+    return res.status(201).send(posts[id])
+})
+
+app.listen(3333, () => console.log("Server is running..."))
