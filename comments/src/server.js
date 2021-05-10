294bec769a2428288d55630059b8e97e0626de79
Marcio Junior <marcio.mj70@gmail.com>
2021-05-10 15:01:24 -0300

    add cors at servers

--- comments/src/server.js
+++ comments/src/server.js
@@ -1,26 +1,28 @@
 const express = require("express")
 const { randomBytes } = require("crypto")
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const commentsByPostId = {}
 
 app.get('/posts/:id/comments', (req, res) => {
-    return res.send(commentsByPostId[req.params.id] || [])
+  return res.send(commentsByPostId[req.params.id] || [])
 })
 
 app.post('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex')
-    const { content } = req.body
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = req.body
 
-    const comments = commentsByPostId[req.params.id] || []
+  const comments = commentsByPostId[req.params.id] || []
 
-    comments.push({ id: commentId, content })
+  comments.push({ id: commentId, content })
 
-    commentsByPostId[req.params.id] = comments
+  commentsByPostId[req.params.id] = comments
 
-    return res.status(201).send(comments)
+  return res.status(201).send(comments)
 })
 
 app.listen(3334, () => console.log('Listening on 3334'))
--- posts/src/server.js
+++ posts/src/server.js
@@ -1,26 +1,28 @@
 const express = require("express")
 const { randomBytes } = require("crypto")
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const posts = {}
 
 app.get('/posts', (req, res) => {
-    return res.send(posts)
+  return res.send(posts)
 })
 
 app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex')
-    const { title } = req.body
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
 
-    posts[id] = {
-        id,
-        title
-    }
+  posts[id] = {
+    id,
+    title
+  }
 
-    return res.status(201).send(posts[id])
+  return res.status(201).send(posts[id])
 })
 
 app.listen(3333, () => console.log("Server is running..."))
