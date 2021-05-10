133eff8e5af5f4ce860390b6ddca548e914d63d3
Marcio Junior <marcio.mj70@gmail.com>
2021-05-10 14:28:57 -0300

    Client side V1

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PostCreate from './PostCreate'
+
+export default () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  )
+}
