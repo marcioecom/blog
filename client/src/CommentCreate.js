716e82a195e9094b52e7ed290a0712bdb826c3a3
Marcio Junior <marcio.mj70@gmail.com>
2021-05-11 09:11:07 -0300

    CommentCreate component

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+import React, { useState } from "react"
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState('')
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    await axios.post(`http://localhost:3334/posts/${postId}/comments`, {
+      content
+    })
+
+    setContent('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            value={content}
+            onChange={e => setContent(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default CommentCreate
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import axios from 'axios'
+import CommentCreate from './CommentCreate'
 
 const PostList = () => {
   const [posts, setPosts] = useState({})
@@ -15,20 +16,25 @@ const PostList = () => {
   }, [])
 
   const renderedPosts = Object.values(posts).map(post => {
-    return <div
-      className="card"
-      style={{ width: "30%", marginBottom: "20px" }}
-      key={post.id}
-    >
-      <div className="card-body">
-        <h3>{post.title}</h3>
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
+        </div>
       </div>
-    </div>
+    )
   })
 
-  return <div className="d-flex flex-row flex-wrap justify-content-between">
-    {renderedPosts}
-  </div>
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  )
 }
 
 export default PostList
